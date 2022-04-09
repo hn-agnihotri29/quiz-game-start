17d161d39579cf1b0db158f73df3eeff9b80f425
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2022-04-09 13:24:23 +0530

    game

---
+++ question_model.py
@@ -0,0 +1,11 @@
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+
+
+q_1 = Question("sdasfafasd", "False")
+
+print(q_1.text)
